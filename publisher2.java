29e973815ffa8daab24e81287354fd4ec75e3486
sanal-py <sanalkavil12@gmail.com>
2026-02-19 15:48:05 +0530

    Add files via upload

---
+++ publisher2.java
@@ -0,0 +1,62 @@
+class Publisher {
+    String publisher;
+
+    Publisher(String publi) {
+        this.publisher = publi;
+    }
+}
+
+class Book {
+    String name;
+    Publisher publisher;
+
+    Book() {}
+
+    public Book(String name, Publisher publisher) {
+        this.name = name;
+        this.publisher = publisher;
+    }
+}
+
+class Literature extends Book {
+    String Lit_type = "Literature";
+
+    Literature(String name, Publisher publisher) {
+        super(name, publisher);
+    }
+
+    void display() {
+        System.out.println("Name: " + super.name);
+        System.out.println("Type: " + this.Lit_type);
+        System.out.println("Publisher: " + this.publisher.publisher);
+    }
+}
+
+class Fiction extends Book {
+    String Lit_type = "Fiction";
+
+    Fiction(String name, Publisher publisher) {
+        super(name, publisher);
+    }
+
+    void display() {
+        System.out.println("Name: " + super.name);
+        System.out.println("Type: " + this.Lit_type);
+        System.out.println("Publisher: " + this.publisher.publisher);
+    }
+}
+
+public class InheritanceBookExample {
+    public static void main(String[] args) {
+
+        Publisher Ip = new Publisher("S.Chand");
+        Literature I = new Literature("As_you_like_it", Ip);
+        I.display();
+
+        System.out.println();
+
+        Publisher fp = new Publisher("Tata_McGraw_Hill");
+        Fiction f = new Fiction("Tempest", fp);
+        f.display();
+    }
+}
